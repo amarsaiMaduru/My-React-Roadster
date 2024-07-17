a7888fdbfb134dfd9d89fe1d065bd54ce9becacd
amarsaiMaduru <167751067+amarsaiMaduru@users.noreply.github.com>
2024-07-17 13:02:30 +0530

    source files of the project

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import './App.css';
+import RegisterForm from './rough/roughForm';
+// import Routeing from './router';
+// import RoughForm from './rough/roughForm';
+// import CounterObject from './stateobject';
+// import ObjectArray from './statewitharray';
+
+// import UseContaxt from './hooks/useeffect';
+// import ReducerCounter from './hooks/usereducer/index1';
+// import TodoPractice from './hooks/todo';
+// import UseMemoo from './hooks/usememo';
+// import Comparision from './useeffectvsusememo';
+// import ComponentB from './hooks/usecontext/components';
+
+
+// import ComponentA from './hooks/usecontext';
+// import ComponentC from './hooks/usecontext/components/indexc';
+// import PracticeAxios from './axios';
+// import CreateForm from './form';
+// import Higher from './hoc';
+// import Hover from './hoc/hover';
+
+//  export const UserContext=createContext()
+function App() {
+  // const object={
+  //   name:"amar"
+  // }
+  // console.log("context",object)
+  return (
+    <div className="App">
+     {/* <CreateForm/> */}
+     {/* <PracticeAxios/> */}
+     {/* <Higher/>
+     <Hover/> */}
+{/* <UserContext.Provider value={object}>
+  <ComponentC/>
+ {/* <ComponentB/> */}
+  {/* </UserContext.Provider> */} 
+  {/* <CounterObject/>
+  <ObjectArray/>
+  <UseContaxt/>
+  <ReducerCounter/>
+  <TodoPractice/>
+  <UseMemoo/>
+  <Comparision/> */}
+  {/* <RoughForm/> */}
+  {/* <Routeing/> */}
+  <RegisterForm/>
+    </div>
+  );
+}
+
+export default App;
